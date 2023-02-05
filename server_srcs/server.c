0d550f4c7ca8a41b014eef4a77f1affa8414d8f6
Raehyeon Jeong <rjeong@student.42seoul.kr>
2023-02-05 13:27:59 +0900

    feat : make server print PID method

--- server_srcs/server.c
+++ server_srcs/server.c
@@ -8,13 +8,17 @@
 /*   Created: 2022/12/14 09:38:35 by rjeong            #+#    #+#             */
 /*   Updated: 2023/01/27 21:47:37 by rjeong           ###   ########.fr       */
 /*                                                                            */
-/* ***********************************************	*************************** */
+/* ************************************************************************** */
 
 #include "../minitalk.h"
 #include "../ft_printf/ft_printf.h"
 
 int	main(void)
 {
-	ft_printf("Sever Started, SEVER PID : %d", getpid());
-//	wait signal
+	ft_printf("\033[36;40mServer start . . .\033[0;31m\nSEVER PID : ");
+	ft_printf("\033[32m%d\033[0m\n", getpid());
+	while (1)
+	{
+		;
+	}
 }
